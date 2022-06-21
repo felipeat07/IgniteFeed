5b6dc0495d0f53fc0edbfeb5f09d88dae29c1ccb
Felipe Tavares <felipeat07@gmail.com>
2022-06-21 13:56:45 +0100

    create a post component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { Header } from "./components/Header";
+
+import './global.css';
+import styles from './app.module.css'
+import { Post } from "./Post";
 
 
-function App() {
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          <Post
+            author='Felipe Tavares'
+            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          />
+
+          <Post
+            author='Diego Fernandes'
+            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          />
+        </main>
+
+
+      </div>
+
+    </>
   )
 }
-
-export default App
