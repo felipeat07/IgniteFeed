44ecae381d5e6903e38b3708edab3909c4fe1d77
Felipe Tavares <felipeat07@gmail.com>
2022-06-28 23:20:18 +0100

    delete comment qnd change state

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar.jsx'
 
-export function Comment(props){
+export function Comment({content, onDeleteComment}){
+    function handleDeleteComment(){
+        onDeleteComment(content)
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/felipeat07.png" alt="" />
@@ -15,13 +19,13 @@ export function Comment(props){
                             <time title='22 de junho de 2022' dateTime='2022-06-22'>Cerca de 1h atrás</time>
                         </div>
 
-                    <button title='Deletar comentário'>
+                    <button onClick={handleDeleteComment} title='Deletar comentário'>
                         <Trash size={24}/>
                     </button>
 
                     </header>
 
-                    <p>{props.content}</p>
+                    <p>{content}</p>
                 </div>        
 
             <footer>
